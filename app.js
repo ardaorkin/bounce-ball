36bf520b5f07ab6020a61cf2780af987e3a926bf
Arda Örkin <ardaorkin@gmail.com>
2021-02-14 11:19:49 +0300

    blocks

--- app.js
+++ app.js
@@ -22,6 +22,19 @@ document.addEventListener("DOMContentLoaded", () => {
   let ballLeft = 250;
   let turnLeft = false;
 
+  let blockOrder = document.createElement("div");
+  let blockOrderHeight = 10;
+  blockOrderTop = 10;
+  blockOrder.style.display = "flex";
+  blockOrder.style.flexDirection = "row";
+  blockOrder.style.width = areaWidth + "px";
+  blockOrder.style.height = blockOrderHeight + "px";
+  blockOrder.style.position = "fixed";
+  blockOrder.style.top = blockOrderTop + "px";
+  area.appendChild(blockOrder);
+
+  let blocks = [];
+
   racket.style.left = racketLeft + "px";
   racket.style.width = racketWidth + "px";
   racket.style.height = racketHeight + "px";
@@ -29,6 +42,39 @@ document.addEventListener("DOMContentLoaded", () => {
   ball.style.left = ballLeft + "px";
   area.style.width = areaWidth + "px";
   area.style.height = areaHeight + "px";
+
+  class BlockOrder {
+    constructor() {
+      this.height = 30;
+      this.width = 30;
+      this.top = 0;
+      this.border = "1px solid black";
+      this.borderRadius = 5;
+      this.backgroundColor = "white";
+      this.position = "relative";
+
+      let blockCount = areaWidth / this.height;
+
+      for (let index = 0; index < blockCount; index++) {
+        const block = document.createElement("div");
+        block.style.width = this.width + "px";
+        block.style.height = this.height + "px";
+        block.style.top = this.top + "px";
+        block.style.backgroundColor = this.backgroundColor;
+        block.style.position = this.position;
+        block.style.left = index * this.width;
+        block.style.border = this.border;
+        block.style.borderRadius = this.borderRadius + "px";
+        blockOrder.appendChild(block);
+      }
+    }
+  }
+
+  function createBlock() {
+    let newOrder = new BlockOrder();
+    blocks.push(newOrder);
+    console.log(blocks);
+  }
 
   function fall() {
     clearInterval(riseInterval);
@@ -57,7 +103,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
       turn();
 
-      if (ballTop === 0) {
+      if (ballTop === blockOrderHeight + blockOrderTop + blockOrderHeight / 2) {
+        alert("Bing");
         fall();
       }
     }, 30);
@@ -111,6 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   if (start) {
+    createBlock();
     fall();
     document.addEventListener("keydown", controls);
   }
